79db03c3c02f54d6375cdcb5a3b72cd36fcc7afe
Nedim <nedim@renesalis-packaging.com>
2024-05-28 23:45:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer, dev, webpack }) => {
-    config.resolve.fallback = {
-      fs: false,
-      net: false,
-      tls: false
-    }
-
-     webpack: (config, { isServer }) => { config.resolve.alias['@'] = path.resolve(__dirname, '.'); return config; },
+webpack: (config, { isServer }) => { config.resolve.alias['@'] = path.resolve(__dirname, '.'); return config; },
   
-    config.externals.push(
-      'pino-pretty',
-      'lokijs',
-      'encoding'
-    )
-
-    return config
-  },
   async redirects() {
     return [
       {
