99852f432133da64d7b217a5dfe3480df10bf1fa
Nedim <nedim@renesalis-packaging.com>
2024-05-29 01:20:38 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer, dev, webpack }) => {
-    config.resolve.fallback = {
-      fs: false,
-      net: false,
-      tls: false
-    }
-
-    config.externals.push(
-      'pino-pretty',
-      'lokijs',
-      'encoding'
-    )
-
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+ 
+    // Important: return the modified config
     return config
   },
   async redirects() {
@@ -28,14 +21,4 @@ const nextConfig = {
 
 module.exports = nextConfig
 
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
- 
-    // Important: return the modified config
-    return config
-  },
-}
 
